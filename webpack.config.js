c75dccec851992b0b04bbc3ad8b3d8f418200d08
TanYiBing <820567197@qq.com>
2018-09-17 15:33:33 +0800

    scss的处理

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
-const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-
+const ExtracTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
   entry: {
       'app': [
@@ -14,18 +12,39 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
   },
-  plugins: [
-      new HtmlWebpackPlugin({
-          filename: 'index.html',
-          template: 'src/templates/index.jade',
-      })
-  ],
   module: {
     rules: [
         {
           test: /\.jade$/,
-          loader: 'jade-loader'
-        }
+          loader: 'jade-loader',
+        },
+        {
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: "css-loader"
+            })
+        },
+        // {
+        //     test: /\.(png|jpg|gif)$/,
+        //     loader: 'url-loader?limit=8192&name=./static/img/[hash].[ext]',
+        // },
+        // {
+        //     // 专供iconfont方案使用的，后面会带一串时间戳，需要特别匹配到
+        //     test: /\.(woff|woff2|svg|eot|ttf)\??.*$/,
+        //     loader: 'file?name=./static/fonts/[name].[ext]',
+        // },
     ]
-},
+    },
+  plugins: [
+      new HtmlWebpackPlugin({
+          filename: 'index.html',
+          template: 'src/templates/index/index.jade',
+          chunks: [],//需要引入哪些js就添加哪些
+          hash: true,
+          minify: true
+      }),
+      new ExtracTextPlugin('style.css'),
+  ],
+  
 };
